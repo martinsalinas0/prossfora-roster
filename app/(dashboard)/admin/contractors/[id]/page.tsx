c790ae78153ae267b1623c614c5ebd7a7969a8e4
Martin <martinlsalinas@pm.me>
2026-09-08 23:49:12 -0500

    feat: implement ContractorDetailPage with contractor data retrieval and not found handling

--- app/(dashboard)/admin/contractors/[id]/page.tsx
+++ app/(dashboard)/admin/contractors/[id]/page.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+import { contractorsData } from "@/lib/data/mockData";
+import { notFound } from "next/navigation";
 
-const ContractorDetailPage = () => {
-  return <div>ContractorDetailPage</div>;
+const ContractorDetailPage = async ({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) => {
+  const { id } = await params;
+
+  const contractor = contractorsData.find((c) => c.id === Number(id));
+
+  if (!contractor) notFound();
+
+  //console.log(contractor?.name);
+  return (
+    <div>
+      <h1>Contractor</h1>
+      <div>{contractor.name}</div>
+    </div>
+  );
 };
 
 export default ContractorDetailPage;
